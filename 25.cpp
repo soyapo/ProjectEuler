9f6b4f76d6aad1cb01e3e9d42a41a14528a43cc7
Hichkas47 <agentvolca47@gmail.com>
2024-10-06 13:13:33 +0330

    new

---
+++ 25.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fst ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+int nums[1010] = {1, 1}, lst;
+int main(){
+    fst
+    while(t--){
+        int mod = 0;
+        for(int i = 0; i <= lst; i++){
+            nums[i] = nums[i] * 2 + mod, mod = nums[i] / 10, nums[i] = nums[i] % 10;
+            if(mod)
+                lst = max(lst, i + 1);
+        }
+    }
+    for(int i = 1000; i >= 0; i--)
+        if(nums[i]) cout<<nums[i];
+    return 0;
+}
